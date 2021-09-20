1dd31ab6ba8c097c841828146be228613275e4e7
Carlos Olivos <cro0010@auburn.edu>
2021-09-19 20:30:51 -0500

    Factorial experiments performed.

--- Genetic_Algorithm_HW2/GA_continuous.py
+++ Genetic_Algorithm_HW2/GA_continuous.py
@@ -25,96 +25,115 @@ y_upper_bound = 32
 
 n_bits = 10
 
-population_size = 50
 n_variables = 2
 
-np.random.seed(4)  # Seed
 pbb_crossover = 0.95
-pbb_mutation = 0.35
-max_generation = 1000
-remain_solutions = 3
 
-np.random.seed(0)
+remain_solutions = 3
 decoding = 2**np.arange(n_bits-1, -1, -1)
 x_correction = correction_factor(x_lower_bound, x_upper_bound, n_bits)
 y_correction = correction_factor(y_lower_bound, y_upper_bound, n_bits)
 
-# %% Initial population
 
-encoding = np.reshape(np.random.choice([0, 1], size=population_size * n_variables * n_bits), (population_size, n_variables, n_bits)) # shape: number solutions, number variables, number of bits
+seed_exp = [i for i in range(9)]
+pbb_mutation_exp = [0.1, 0.25, 0.6]
+population_size_exp = [10, 40, 100]
+max_generation_exp = [500]
 
-population = {i : encoding[i] for i in range(population_size)}
-population_fitness = {i : z_function(sum(decoding * encoding[i][0]) * x_correction + x_lower_bound, sum(decoding * encoding[i][1]) * y_correction + y_lower_bound) for i in range(population_size)}
-print(min(population_fitness.values()))
 
 # %%
+for max_generation_it in max_generation_exp:
+    output_saver = {}
+    for seed_it in seed_exp:
+        for pbb_mutation_it in pbb_mutation_exp:
+            for pop_size_it in population_size_exp:
 
-for generation_iterator in range(max_generation):
+                pbb_mutation = pbb_mutation_it
+                population_size = pop_size_it
+                max_generation = max_generation_it
+                np.random.seed(seed_it)
+                encoding = np.reshape(np.random.choice([0, 1], size=population_size * n_variables * n_bits), (population_size, n_variables, n_bits)) # shape: number solutions, number variables, number of bits
 
-    children = {}
-    children_counter = 0
-    for parents in range(int(population_size / 2)):
+                population = {i : encoding[i] for i in range(population_size)}
+                population_fitness = {i : z_function(sum(decoding * encoding[i][0]) * x_correction + x_lower_bound, sum(decoding * encoding[i][1]) * y_correction + y_lower_bound) for i in range(population_size)}
 
-        # Tournament
-        # print('torunament')
-        candidates_tourn = np.random.choice([i for i in range(population_size)], size=2, replace=False)
-        index_candidate = np.argmin(np.array([population_fitness[candidates_tourn[0]], population_fitness[candidates_tourn[1]]]))
+                iteration_saver = {}
+                iteration = 0
+                # GA starts
+                for generation_iterator in range(max_generation):
+                    children = {}
+                    children_counter = 0
 
-        first_parent_index = candidates_tourn[index_candidate]
-        second_parent_index = np.random.choice([i for i in range(population_size) if i != first_parent_index])
+                    min_sol_index = np.argmin(population_fitness.values())
+                    min_sol_x = sum(decoding * population[min_sol_index][0]) * x_correction + x_lower_bound
+                    min_sol_y = sum(decoding * population[min_sol_index][1]) * y_correction + y_lower_bound
+                    #iteration_saver[iteration] = min(population_fitness.values()), min_sol_x.copy(), min_sol_y.copy()
+                    iteration_saver[iteration] = min(population_fitness.values())
+                    iteration += 1
+                    for parents in range(int(population_size / 2)):
 
-        # Crossover
-        # One point crossover
-        if np.random.rand() <= pbb_crossover:
+                        # Tournament
+                        # print('torunament')
+                        candidates_tourn = np.random.choice([i for i in range(population_size)], size=2, replace=False)
+                        index_candidate = np.argmin(np.array([population_fitness[candidates_tourn[0]], population_fitness[candidates_tourn[1]]]))
 
-            # Selecting crossover point
-            point_selection_crossover = np.random.randint(1, n_bits - 1, 1)[0]
+                        first_parent_index = candidates_tourn[index_candidate]
+                        second_parent_index = np.random.choice([i for i in range(population_size) if i != first_parent_index])
 
-            # Creating first half offsprings
-            offspring_first = np.array([np.concatenate([population[first_parent_index][0][0:point_selection_crossover].copy(), population[second_parent_index][0][point_selection_crossover:].copy()]),
-                                          np.concatenate([population[first_parent_index][1][0:point_selection_crossover].copy(), population[second_parent_index][1][point_selection_crossover:].copy()])])
+                        # Crossover
+                        # One point crossover
+                        if np.random.rand() <= pbb_crossover:
 
-            offspring_second = np.array([np.concatenate([population[second_parent_index][0][0:point_selection_crossover].copy(), population[first_parent_index][0][point_selection_crossover:].copy()]),
-                                          np.concatenate([population[second_parent_index][1][0:point_selection_crossover].copy(), population[first_parent_index][1][point_selection_crossover:].copy()])])
+                            # Selecting crossover point
+                            point_selection_crossover = np.random.randint(1, n_bits - 1, 1)[0]
 
-        else:
-            offspring_first, offspring_second = population[first_parent_index].copy(), population[second_parent_index].copy()
+                            # Creating first half offsprings
+                            offspring_first = np.array([np.concatenate([population[first_parent_index][0][0:point_selection_crossover].copy(), population[second_parent_index][0][point_selection_crossover:].copy()]),
+                                                          np.concatenate([population[first_parent_index][1][0:point_selection_crossover].copy(), population[second_parent_index][1][point_selection_crossover:].copy()])])
 
-        # Mutation
-        random_number_evaluation = np.random.random(size=offspring_first.shape)
-        mask_mutation = random_number_evaluation < pbb_mutation
-        offspring_first_m = offspring_first.copy()
-        offspring_first_m[mask_mutation] = np.where((offspring_first_m[mask_mutation] == 0) | (offspring_first_m[mask_mutation] == 1), offspring_first_m[mask_mutation] ^ 1, offspring_first_m[mask_mutation])
+                            offspring_second = np.array([np.concatenate([population[second_parent_index][0][0:point_selection_crossover].copy(), population[first_parent_index][0][point_selection_crossover:].copy()]),
+                                                          np.concatenate([population[second_parent_index][1][0:point_selection_crossover].copy(), population[first_parent_index][1][point_selection_crossover:].copy()])])
 
-        random_number_evaluation = np.random.random(size=offspring_second.shape)
-        mask_mutation = random_number_evaluation < pbb_mutation
-        offspring_second_m = offspring_second.copy()
-        offspring_second_m[mask_mutation] = np.where((offspring_second_m[mask_mutation] == 0) | (offspring_second_m[mask_mutation] == 1), offspring_second_m[mask_mutation] ^ 1, offspring_second_m[mask_mutation])
+                        else:
+                            offspring_first, offspring_second = population[first_parent_index].copy(), population[second_parent_index].copy()
 
-        # Adding new children
-        children[children_counter] = offspring_first_m.copy()
-        children_counter += 1
-        children[children_counter] = offspring_second_m.copy()
-        children_counter += 1
+                        # Mutation
+                        random_number_evaluation = np.random.random(size=offspring_first.shape)
+                        mask_mutation = random_number_evaluation < pbb_mutation
+                        offspring_first_m = offspring_first.copy()
+                        offspring_first_m[mask_mutation] = np.where((offspring_first_m[mask_mutation] == 0) | (offspring_first_m[mask_mutation] == 1), offspring_first_m[mask_mutation] ^ 1, offspring_first_m[mask_mutation])
 
-    # Elitism
-    children_fitness = {i: z_function(sum(decoding * children[i][0]) * x_correction + x_lower_bound, sum(decoding * children[i][1]) * y_correction + y_lower_bound) for i in
-                        range(len(children))}
+                        random_number_evaluation = np.random.random(size=offspring_second.shape)
+                        mask_mutation = random_number_evaluation < pbb_mutation
+                        offspring_second_m = offspring_second.copy()
+                        offspring_second_m[mask_mutation] = np.where((offspring_second_m[mask_mutation] == 0) | (offspring_second_m[mask_mutation] == 1), offspring_second_m[mask_mutation] ^ 1, offspring_second_m[mask_mutation])
 
-    new_generation_children = sorted(children_fitness, key=children_fitness.get, reverse=False)[
-                              :population_size - remain_solutions]
-    new_generation_parent = sorted(population_fitness, key=population_fitness.get, reverse=False)[:remain_solutions]
+                        # Adding new children
+                        children[children_counter] = offspring_first_m.copy()
+                        children_counter += 1
+                        children[children_counter] = offspring_second_m.copy()
+                        children_counter += 1
 
-    tuple_prev_parents = [([i for i in range(len(new_generation_parent))][j], new_generation_parent[j]) for j in
-                          range(remain_solutions)]
-    tuple_new_chi = [([i for i in range(remain_solutions, population_size)][j], new_generation_children[j]) for j in
-                     range(population_size - remain_solutions)]
-    #pr0int('replacement')
-    population = {i: population[j].copy() for i, j in tuple_prev_parents}
-    for i, j in tuple_new_chi:
-        population[i] = children[j].copy()
+                    # Elitism
+                    children_fitness = {i: z_function(sum(decoding * children[i][0]) * x_correction + x_lower_bound, sum(decoding * children[i][1]) * y_correction + y_lower_bound) for i in
+                                        range(len(children))}
 
-    # population = children.copy()
+                    new_generation_children = sorted(children_fitness, key=children_fitness.get, reverse=False)[
+                                              :population_size - remain_solutions]
+                    new_generation_parent = sorted(population_fitness, key=population_fitness.get, reverse=False)[:remain_solutions]
 
-    population_fitness = {i: z_function(sum(decoding * population[i][0]) * x_correction + x_lower_bound, sum(decoding * population[i][1]) * y_correction + y_lower_bound) for i in range(population_size)}
-    print(min(population_fitness.values()))
+                    tuple_prev_parents = [([i for i in range(len(new_generation_parent))][j], new_generation_parent[j]) for j in
+                                          range(remain_solutions)]
+                    tuple_new_chi = [([i for i in range(remain_solutions, population_size)][j], new_generation_children[j]) for j in
+                                     range(population_size - remain_solutions)]
+
+                    population = {i: population[j].copy() for i, j in tuple_prev_parents}
+                    for i, j in tuple_new_chi:
+                        population[i] = children[j].copy()
+
+                    population_fitness = {i: z_function(sum(decoding * population[i][0]) * x_correction + x_lower_bound, sum(decoding * population[i][1]) * y_correction + y_lower_bound) for i in range(population_size)}
+
+                output_saver[f'{seed_it}_{max_generation_it}_{pop_size_it}_{pbb_mutation_it}'] = iteration_saver
+
+    #pd.DataFrame(output_saver).to_excel(f'Genetic_Algorithm_HW2/continuous_ga_{max_generation_it}.xlsx')
+    pd.DataFrame(output_saver).to_excel(f'continuous_ga_{max_generation_it}_233.xlsx')
--- Genetic_Algorithm_HW2/QAP_GA.py
+++ Genetic_Algorithm_HW2/QAP_GA.py
@@ -17,12 +17,12 @@ def objective_function(distance, flow, departments_set_square, department_assign
 
 # %%
 # Import data
-df_distance = pd.read_excel('Genetic_Algorithm_HW2/data_qap.xlsx', sheet_name='Distance', header=None)
-# df_distance = pd.read_excel('data_qap.xlsx', sheet_name = 'Distance', header=None)
+#df_distance = pd.read_excel('Genetic_Algorithm_HW2/data_qap.xlsx', sheet_name='Distance', header=None)
+df_distance = pd.read_excel('data_qap.xlsx', sheet_name = 'Distance', header=None)
 distance_matrix = df_distance.to_numpy()
 
-df_flow = pd.read_excel('Genetic_Algorithm_HW2/data_qap.xlsx', sheet_name='Flow', header=None)
-# df_flow = pd.read_excel('data_qap.xlsx', sheet_name = 'Flow', header=None)
+#df_flow = pd.read_excel('Genetic_Algorithm_HW2/data_qap.xlsx', sheet_name='Flow', header=None)
+df_flow = pd.read_excel('data_qap.xlsx', sheet_name = 'Flow', header=None)
 flow_matrix = df_flow.to_numpy()
 
 n_departments = len(distance_matrix)
@@ -39,124 +39,143 @@ arcs = np.array([(i, j) for i in range(n_departments) for j in range(n_departmen
 # max_generation = 1000
 # remain_solutions = 2
 
-np.random.seed(4)  # Seed
 pbb_crossover = 0.95
-pbb_mutation = 0.4
-population_size = 200
-max_generation = 1000
 remain_solutions = 2
-# %% Create population
+seed_exp = [i for i in range(9)]
+pbb_mutation_exp = [0.1, 0.4, 0.6]
+population_size_exp = [50, 100, 300]
+max_generation_exp = [200]
 
+# %% Create population
 departments_set = np.array([i for i in range(n_departments)])
 location_set = np.array([i for i in range(n_departments)])
 
-# Generation initial solutions
-population = {}
-for creation_sol_iterator in range(population_size):
-    random_solution = shuffle(location_set)
-    population[creation_sol_iterator] = np.array([random_solution[i] for i in range(n_departments)])
+for max_generation_it in max_generation_exp:
+    output_saver = {}
+    for seed_it in seed_exp:
+        for pbb_mutation_it in pbb_mutation_exp:
+            for pop_size_it in population_size_exp:
 
-# np.array([shuffle(location_set) for i in range(10)])[1] using array
-# Compute fitness for the initial population
-population_fitness = {i: objective_function(distance_matrix, flow_matrix, arcs, population[i]) for i in
-                      range(population_size)}
+                np.random.seed(seed_it)  # Seed
+                pbb_mutation = pbb_mutation_it
+                population_size = pop_size_it
+                max_generation = max_generation_it
 
-# %%
+                iteration_saver = {}
+                iteration = 0
 
-for generation_iterator in range(max_generation):
-    # print(generation_iterator)
-    children = {}
-    children_counter = 0
-    for parents in range(int(population_size / 2)):
+                # Generation initial solutions
+                population = {}
+                for creation_sol_iterator in range(population_size):
+                    random_solution = shuffle(location_set)
+                    population[creation_sol_iterator] = np.array([random_solution[i] for i in range(n_departments)])
 
-        # Tournament
-        # print('torunament')
-        candidates_tourn = np.random.choice([i for i in range(population_size)], size=2, replace=False)
-        index_candidate = np.argmin(
-            np.array([population_fitness[candidates_tourn[0]], population_fitness[candidates_tourn[1]]]))
+                # np.array([shuffle(location_set) for i in range(10)])[1] using array
+                # Compute fitness for the initial population
+                population_fitness = {i: objective_function(distance_matrix, flow_matrix, arcs, population[i]) for i in
+                                      range(population_size)}
 
-        first_parent_index = candidates_tourn[index_candidate]
-        second_parent_index = np.random.choice([i for i in range(population_size) if i != first_parent_index])
+                # %%
 
-        # Crossover
-        # One point crossover
-        # print('crossover')
-        if np.random.rand() <= pbb_crossover:
+                for generation_iterator in range(max_generation):
+                    # print(generation_iterator)
+                    children = {}
+                    children_counter = 0
 
-            # Selecting crossover point
-            point_selection_crossover = np.random.randint(1, len(location_set) - 1, 1)[0]
+                    iteration_saver[iteration] = min(population_fitness.values())
+                    iteration += 1
 
-            # Creating first half offsprings
-            offspring_first = population[first_parent_index][0:point_selection_crossover].copy()
-            second_half_first_parent = population[first_parent_index][point_selection_crossover:].copy()
 
-            offspring_second = population[second_parent_index][0:point_selection_crossover].copy()
-            second_half_second_parent = population[second_parent_index][point_selection_crossover:].copy()
+                    for parents in range(int(population_size / 2)):
 
-            # Creating the first offspring
-            second_half_second_parent = second_half_second_parent[~np.in1d(second_half_second_parent, offspring_first)]
-            offspring_first = np.concatenate([offspring_first, second_half_second_parent])
+                        # Tournament
+                        # print('torunament')
+                        candidates_tourn = np.random.choice([i for i in range(population_size)], size=2, replace=False)
+                        index_candidate = np.argmin(
+                            np.array([population_fitness[candidates_tourn[0]], population_fitness[candidates_tourn[1]]]))
 
-            missing_in_first_offspring = population[first_parent_index][
-                ~np.in1d(population[first_parent_index], offspring_first)]
-            offspring_first = np.concatenate([offspring_first, missing_in_first_offspring])
+                        first_parent_index = candidates_tourn[index_candidate]
+                        second_parent_index = np.random.choice([i for i in range(population_size) if i != first_parent_index])
 
-            # Creating the second offspring
-            second_half_first_parent = second_half_first_parent[~np.in1d(second_half_first_parent, offspring_second)]
-            offspring_second = np.concatenate([offspring_second, second_half_first_parent])
+                        # Crossover
+                        # One point crossover
+                        # print('crossover')
+                        if np.random.rand() <= pbb_crossover:
 
-            missing_in_second_offspring = population[second_parent_index][
-                ~np.in1d(population[second_parent_index], offspring_second)]
-            offspring_second = np.concatenate([offspring_second, missing_in_second_offspring])
+                            # Selecting crossover point
+                            point_selection_crossover = np.random.randint(1, len(location_set) - 1, 1)[0]
 
-        else:
-            offspring_first, offspring_second = population[first_parent_index].copy(), population[
-                second_parent_index].copy()
+                            # Creating first half offsprings
+                            offspring_first = population[first_parent_index][0:point_selection_crossover].copy()
+                            second_half_first_parent = population[first_parent_index][point_selection_crossover:].copy()
 
-        # Mutation
-        if np.random.rand() <= pbb_mutation:
+                            offspring_second = population[second_parent_index][0:point_selection_crossover].copy()
+                            second_half_second_parent = population[second_parent_index][point_selection_crossover:].copy()
 
-            index_swap = np.random.randint(-14, 1, 1)[0]
-            direction_swap = np.random.choice([-1, 1], 1)[0]
+                            # Creating the first offspring
+                            second_half_second_parent = second_half_second_parent[~np.in1d(second_half_second_parent, offspring_first)]
+                            offspring_first = np.concatenate([offspring_first, second_half_second_parent])
 
-            offspring_first[index_swap], offspring_first[index_swap + direction_swap] = offspring_first[
-                                                                                            index_swap + direction_swap], \
-                                                                                        offspring_first[index_swap]
+                            missing_in_first_offspring = population[first_parent_index][
+                                ~np.in1d(population[first_parent_index], offspring_first)]
+                            offspring_first = np.concatenate([offspring_first, missing_in_first_offspring])
 
-        if np.random.rand() <= pbb_mutation:
-            index_swap = np.random.randint(-14, 1, 1)[0]
-            direction_swap = np.random.choice([-1, 1], 1)[0]
+                            # Creating the second offspring
+                            second_half_first_parent = second_half_first_parent[~np.in1d(second_half_first_parent, offspring_second)]
+                            offspring_second = np.concatenate([offspring_second, second_half_first_parent])
 
-            offspring_second[index_swap], offspring_second[index_swap + direction_swap] = offspring_second[
-                                                                                              index_swap + direction_swap], \
-                                                                                          offspring_second[index_swap]
+                            missing_in_second_offspring = population[second_parent_index][
+                                ~np.in1d(population[second_parent_index], offspring_second)]
+                            offspring_second = np.concatenate([offspring_second, missing_in_second_offspring])
 
-        # %%
-        # print('replacement')
-        children[children_counter] = offspring_first.copy()
-        children_counter += 1
-        children[children_counter] = offspring_second.copy()
-        children_counter += 1
+                        else:
+                            offspring_first, offspring_second = population[first_parent_index].copy(), population[
+                                second_parent_index].copy()
 
-    # %% Elitism
-    children_fitness = {i: objective_function(distance_matrix, flow_matrix, arcs, children[i]) for i in
-                        range(len(children))}
+                        # Mutation
+                        if np.random.rand() <= pbb_mutation:
 
-    new_generation_children = sorted(children_fitness, key=children_fitness.get, reverse=False)[
-                              :population_size - remain_solutions]
-    new_generation_parent = sorted(population_fitness, key=population_fitness.get, reverse=False)[:remain_solutions]
+                            index_swap = np.random.randint(-14, 1, 1)[0]
+                            direction_swap = np.random.choice([-1, 1], 1)[0]
 
-    tuple_prev_parents = [([i for i in range(len(new_generation_parent))][j], new_generation_parent[j]) for j in
-                          range(remain_solutions)]
-    tuple_new_chi = [([i for i in range(remain_solutions, population_size)][j], new_generation_children[j]) for j in
-                     range(population_size - remain_solutions)]
+                            offspring_first[index_swap], offspring_first[index_swap + direction_swap] = offspring_first[
+                                                                                                            index_swap + direction_swap], \
+                                                                                                        offspring_first[index_swap]
 
-    population = {i: population[j].copy() for i, j in tuple_prev_parents}
-    for i, j in tuple_new_chi:
-        population[i] = children[j].copy()
+                        if np.random.rand() <= pbb_mutation:
+                            index_swap = np.random.randint(-14, 1, 1)[0]
+                            direction_swap = np.random.choice([-1, 1], 1)[0]
 
-    # population = children.copy()
+                            offspring_second[index_swap], offspring_second[index_swap + direction_swap] = offspring_second[
+                                                                                                              index_swap + direction_swap], \
+                                                                                                          offspring_second[index_swap]
 
-    population_fitness = {i: objective_function(distance_matrix, flow_matrix, arcs, population[i]) for i in
-                          range(len(population))}
-    print(min(population_fitness.values()))
+                        children[children_counter] = offspring_first.copy()
+                        children_counter += 1
+                        children[children_counter] = offspring_second.copy()
+                        children_counter += 1
+
+                    # %% Elitism
+                    children_fitness = {i: objective_function(distance_matrix, flow_matrix, arcs, children[i]) for i in
+                                        range(len(children))}
+
+                    new_generation_children = sorted(children_fitness, key=children_fitness.get, reverse=False)[
+                                              :population_size - remain_solutions]
+                    new_generation_parent = sorted(population_fitness, key=population_fitness.get, reverse=False)[:remain_solutions]
+
+                    tuple_prev_parents = [([i for i in range(len(new_generation_parent))][j], new_generation_parent[j]) for j in
+                                          range(remain_solutions)]
+                    tuple_new_chi = [([i for i in range(remain_solutions, population_size)][j], new_generation_children[j]) for j in
+                                     range(population_size - remain_solutions)]
+
+                    population = {i: population[j].copy() for i, j in tuple_prev_parents}
+                    for i, j in tuple_new_chi:
+                        population[i] = children[j].copy()
+
+                    population_fitness = {i: objective_function(distance_matrix, flow_matrix, arcs, population[i]) for i in
+                                          range(len(population))}
+
+                    output_saver[f'{seed_it}_{max_generation_it}_{pop_size_it}_{pbb_mutation_it}'] = iteration_saver
+
+    pd.DataFrame(output_saver).to_excel(f'qap_ga_{max_generation_it}.xlsx')
+
